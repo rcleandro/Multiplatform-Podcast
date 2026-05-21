041974cf1573608fc574b44987918c217cb5dca8
Leandro Carvalho <sj.leandro@gmail.com>
2026-05-21 10:07:58 -0300

    fix(ios): initialize koin and correct mainviewcontroller return type

--- shared/src/iosMain/kotlin/br/com/carvalho/podcast/MainViewController.kt
+++ shared/src/iosMain/kotlin/br/com/carvalho/podcast/MainViewController.kt
@@ -4,13 +4,17 @@ import androidx.compose.ui.window.ComposeUIViewController
 import br.com.carvalho.podcast.presentation.navigation.RootComponentImpl
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
-
 import androidx.compose.runtime.remember
 import kotlinx.coroutines.FlowPreview
+import br.com.carvalho.podcast.core.di.initKoin
+import platform.UIKit.UIViewController
 
 @OptIn(FlowPreview::class)
-fun MainViewController() = ComposeUIViewController {
-    val lifecycle = remember { LifecycleRegistry() }
-    val root = remember { RootComponentImpl(DefaultComponentContext(lifecycle = lifecycle)) }
-    App(root)
+fun MainViewController(): UIViewController {
+    initKoin()
+    return ComposeUIViewController {
+        val lifecycle = remember { LifecycleRegistry() }
+        val root = remember { RootComponentImpl(DefaultComponentContext(lifecycle = lifecycle)) }
+        App(root)
+    }
 }
